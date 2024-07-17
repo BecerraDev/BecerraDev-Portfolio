8ec9e2d7f33674385c9b097fc6193d15afe8b3b3
BecerraDev <byron.becerra.aravena@gmail.com>
2024-07-17 03:41:43 -0400

    Se realiza cambio a vite config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,34 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue"; //add this line
-
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
-        vue()
-        
+        vue(),
     ],
 
     resolve: {
         alias: {
-            'vue': 'vue/dist/vue.esm-bundler.js',
-            '$': 'jquery',
-            'jQuery': 'jquery'
-        }
-    }
-
+            vue: "vue/dist/vue.esm-bundler.js",
+            $: "jquery",
+            jQuery: "jquery",
+        },
+    },
+    build: {
+        // Directorio de salida para los archivos compilados
+        outDir: "public/build",
+        // Generar el archivo manifest.json
+        manifest: true,
+        // Opciones de Rollup para configurar las entradas
+        rollupOptions: {
+            input: {
+                app: "resources/js/app.js", // Entrada principal de JavaScript
+                styles: "resources/css/app.css", // Entrada principal de CSS
+            },
+        },
+    },
 });
